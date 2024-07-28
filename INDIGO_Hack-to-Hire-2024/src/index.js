08063fcfd897df256c7ac4ee02b50b85af09b8ba
Raj Anand Soni <113697967+rajsoni06@users.noreply.github.com>
2024-07-28 12:44:43 +0530

    FlightStatus.js

--- INDIGO_Hack-to-Hire-2024/src/index.js
+++ INDIGO_Hack-to-Hire-2024/src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const FlightStatus = () => {
+  const [flights, setFlights] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/flights/')
+      .then(response => setFlights(response.data))
+      .catch(error => console.error('Error fetching flight data:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Flight Status</h1>
+      <ul>
+        {flights.map(flight => (
+          <li key={flight._id}>
+            Flight {flight.number} - Status: {flight.status} - Gate: {flight.gate}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FlightStatus;
